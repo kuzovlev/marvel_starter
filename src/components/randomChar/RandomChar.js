9db8c4b696c5be17b5ff76b55e220ccaa1c6443b
kuzovlev <kuzovlev.dm@gmail.com>
2022-01-19 14:32:07 +0200

    Исправлен баг с дублирование запроса в базу, поправлены стили при отсутсвующей картинке персонажа

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -6,10 +6,6 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         char: {},
@@ -18,6 +14,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount(){
+        this.updateChar();
+    }
 
     onCharLoaded = (char) => {
         this.setState(
@@ -63,7 +63,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -75,9 +75,10 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
     const { name, thumbnail, description, homepage, wiki } = char;
+    const thumbnailClass = thumbnail.includes('image_not_available')?'randomchar__img unavailable':'randomchar__img';
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <img src={thumbnail} alt="Random character" className={thumbnailClass} />
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
