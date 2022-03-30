0a940ee29e707f74ec3d2931ba39c6a060aa5bc6
Dmytro Kuzovlev <kuzovlev.dm@gmail.com>
2022-03-30 18:46:54 +0300

    Routing added to project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,44 +1,24 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {MainPage, ComicsPage} from "../pages";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import AppBanner from '../appBanner/AppBanner';
-
-import decoration from '../../resources/img/vision.png';
-import ComicsList from "../comicsList/ComicsList";
 
 const App = () => {
-
-    const [selectedChar, setSelectedChar] = useState(null);
-
-
-    const onCharSelected = (id) => {
-        setSelectedChar(id);
-    }
-
-
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <AppBanner/>
-                <ComicsList/>
-                {/* <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
+        <Router>
+            <Switch>
+                <div className="app">
+                    <AppHeader />
+                    <main>
+                        <Route  exact path={'/comics'}>
+                            <ComicsPage/>
+                        </Route>
+                        <Route exact path={'/'}>
+                            <MainPage/>
+                        </Route>
+                    </main>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-            </main>
-        </div>
+            </Switch>
+        </Router>
     )
 }
 
